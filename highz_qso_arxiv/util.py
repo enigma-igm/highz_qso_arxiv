c797805616129eb55b6549ca1c1ac1abfba195c1
topologyYDM <topologyYDM@gmail.com>
2022-06-02 23:52:10 +0200

    add utils.py

---
+++ highz_qso_arxiv/util.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+def ivarsmooth(flux, ivar, window):
+    """
+    Boxcar smoothign of width window with ivar weights
+    Args:
+        flux:
+        ivar:
+        window:
+    Returns:
+    """
+
+    nflux = (flux.shape)[0]
+    halfwindow = int(np.floor((np.round(window) - 1)/2))
+    shiftarr = np.zeros((nflux, 2*halfwindow + 1))
+    shiftivar = np.zeros((nflux, 2*halfwindow + 1))
+    shiftindex = np.zeros((nflux, 2*halfwindow + 1))
+    indexarr = np.arange(nflux)
+    indnorm = np.outer(indexarr,(np.zeros(2 *halfwindow + 1) + 1))
+
+    for i in np.arange(-halfwindow,halfwindow + 1,dtype=int):
+        shiftarr[:,i+halfwindow] = np.roll(flux,i)
+        shiftivar[:, i+halfwindow] = np.roll(ivar, i)
+        shiftindex[:, i+halfwindow] = np.roll(indexarr, i)
+
+    wh = (np.abs(shiftindex - indnorm) > (halfwindow+1))
+    shiftivar[wh]=0.0
+
+    outivar = np.sum(shiftivar,axis=1)
+    nzero, = np.where(outivar > 0.0)
+    zeroct=len(nzero)
+    smoothflux = np.sum(shiftarr * shiftivar, axis=1)
+    if(zeroct > 0):
+        smoothflux[nzero] = smoothflux[nzero]/outivar[nzero]
+    else:
+        smoothflux = np.roll(flux, 2*halfwindow + 1) # kill off NAN's
+
+    return smoothflux, outivar
