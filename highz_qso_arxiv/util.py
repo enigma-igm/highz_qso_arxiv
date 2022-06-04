2437c11559bdc5e361ff9692cc651041df03db75
topologyYDM <topologyYDM@gmail.com>
2022-06-04 14:05:28 +0200

    add get_skyprobe_extinction to util.py

--- highz_qso_arxiv/util.py
+++ highz_qso_arxiv/util.py
@@ -1,4 +1,10 @@
+import imp
 import numpy as np
+from astropy.time import Time
+from astropy.table import Table
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
 from IPython import embed
 
 def inverse(array):
@@ -40,3 +46,60 @@ def ivarsmooth(flux, ivar, window):
         smoothflux = np.roll(flux, 2*halfwindow + 1) # kill off NAN's
 
     return smoothflux, outivar
+
+def mjd_to_unix(t_in_mjd):
+    t = Time(t_in_mjd, format="mjd")
+    return t.unix
+
+def mjd_to_iso(t_in_mjd):
+    t = Time(t_in_mjd, format="mjd")
+    return t.iso
+
+def unix_to_mjd(t_in_unix):
+    t = Time(t_in_unix, format="unix")
+    return t.mjd
+
+def unix_to_iso(t_in_unix):
+    t = Time(t_in_unix, format="unix")
+    return t.iso
+
+def get_skyprobe_extinction(t_start, t_end, format="unix"):
+    """get extinction data from SkyProbe
+       link: https://www.cfht.hawaii.edu/Instruments/Elixir/skyprobe/archive-new.html
+
+        NOTE: there is a small, but variable, zero-point offset to the extinction measurement. 
+              This varies based on sky conditions, instrument cleanliness (dust on the lens), 
+              and telescope airmass. 
+              For simplicity's sake, CFHT staff usually assume an average offset of +0.03.
+              Thus we probably should -0.03 from the extinction we get.
+    Args:
+        t_start (int): UNIX time
+        t_end (int):UNIX time
+    """
+
+    if format != "unix":
+        if format != "mjd":
+            raise TypeError("Time format unsupported")
+        elif format == "mjd":
+            t_start = mjd_to_unix(t_start)
+            t_end = mjd_to_unix(t_end)
+
+    url = f"https://www.cfht.hawaii.edu/ObsInfo/Weather/current/skyprobe_data.php?b={t_start}&e={t_end}"
+    html = urlopen(url, timeout=10)
+    soup = BeautifulSoup(html, 'html.parser')
+    table_str = soup.get_text()
+
+    # turn string into actual table (or list)
+    table_str = table_str.replace("\n","")
+    table_str = table_str.split("\r")
+    table = np.array([r.split(",") for r in table_str])
+
+    t_unix = table[:,0].astype("int") # UNIX time
+    extinction = table[:,1].astype("float") # SkyProbe extinction measurement, in mag
+    scatter = table[:,2].astype("float") # scatter of measurement, in mag
+
+    # construct Table
+    data = Table([t_unix, extinction, scatter], 
+                 names=("Time", "Extinction", "Scatter"), 
+                 units=("UNIX", "mag", "mag"))
+    return data
