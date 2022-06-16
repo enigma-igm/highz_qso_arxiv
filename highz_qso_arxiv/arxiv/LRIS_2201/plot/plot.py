22d091536445ab9a003bbca1f17e60cee8513d34
topologyYDM <topologyYDM@gmail.com>
2022-06-16 10:21:49 +0200

    update LRIS 2201 plots

--- highz_qso_arxiv/arxiv/LRIS_2201/plot/plot.py
+++ highz_qso_arxiv/arxiv/LRIS_2201/plot/plot.py
@@ -1,5 +1,8 @@
 from highz_qso_arxiv.plot import plot_series
 
+"""
+    plot all without tellruic
+"""
 name_list = ['J0759+2811', 'J0739+2328', 'J0820+2412', 'J0831+2558',
              'J0849+2601', 'J0936+3244', 'J0936+3346', 'J0938+3332',
              'J1030-0031', 'J1003-2610', 'J1145+0933', 'J1146+0039',
@@ -11,3 +14,21 @@ fits_list[12] = f"../reduced/all/coadd2d/{name_list[12]}_coadd.fits"
 fits_list[15] = f"../reduced/all/coadd2d/{name_list[15]}_coadd.fits"
 idx_list = [1 for i in range(len(name_list))]
 plot_series(name_list, fits_list, idx_list, display=False, save_file="LRIS_2201.pdf")
+
+"""
+    plot all with telluric
+"""
+fits_list = [f"../reduced/all/{nm}/{nm}_coadd_tellcorr.fits" for nm in name_list]
+fits_list[10] = f"../reduced/all/coadd2d/{name_list[10]}_coadd_tellcorr.fits"
+fits_list[12] = f"../reduced/all/coadd2d/{name_list[12]}_coadd_tellcorr.fits"
+fits_list[15] = f"../reduced/all/coadd2d/{name_list[15]}_coadd_tellcorr.fits"
+idx_list = [1 for i in range(len(name_list))]
+plot_series(name_list, fits_list, idx_list, telluric=True, display=False, save_file="LRIS_2201_tellcorr.pdf")
+
+"""
+    plot J1206 and J1256
+"""
+name_list = ['J1206-0051', 'J1256-0306']
+fits_list = [f"../reduced/all/coadd2d/{nm}_coadd_tellcorr.fits" for nm in name_list]
+idx_list = [1 for i in range(len(name_list))]
+plot_series(name_list, fits_list, idx_list, telluric=True, display=False, save_file="J1206_J1256.pdf")
