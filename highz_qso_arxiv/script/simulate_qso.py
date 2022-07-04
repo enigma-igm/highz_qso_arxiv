c0dd3e38a10a2e2067dcfa85a1b853bea4b44c91
topologyYDM <topologyYDM@gmail.com>
2022-07-04 11:42:42 +0200

    qso simu fix

--- highz_qso_arxiv/script/simulate_qso.py
+++ highz_qso_arxiv/script/simulate_qso.py
@@ -65,7 +65,7 @@ def redshift_to_distance(redshift) -> float:
     """
     return cosmo.luminosity_distance(redshift).value
     
-def simulate_Nlam(model='qso', redshift=None, M_1450=None, 
+def simulate_Nlam(model='qso', redshift=None, m_1450=None, M_1450=None, 
                   star_scale=None, star_type=None, debug=False):
     # read in quasar spectrum from Selsing 2015, in erg/s/cm2/A
     # shift the spectrum to the observe frame and cut off at the Lyman-alpha
@@ -76,22 +76,28 @@ def simulate_Nlam(model='qso', redshift=None, M_1450=None,
     # 2. scale for star template (use magnitude)
     # 3. quasar absorption
 
-    if model == 'qso':        
-        dL = redshift_to_distance(redshift)
-        mAB = M_1450 + 5 * np.log10(dL*1e6/10)
+    if model == 'qso':
+        embed()
+        if m_1450 != None:
+            mAB = m_1450
+        elif M_1450 != None:
+            dL = redshift_to_distance(redshift)
+            mAB = M_1450 + 5 * np.log10(dL*1e6/10)
+        else:
+            raise ValueError('m_1450 or M_1450 must be specified')
         mAB = mAB * u.ABmag
         flam = mAB.to(u.erg/u.s/u.cm**2/u.AA, u.spectral_density(1450*u.AA))
 
         dat = ascii.read("../resource/Selsing2015.dat")
         wl_rest = dat["col1"]
         wl_obs = wl_rest * (1 + redshift)
-        flux = dat["col2"] # in 1e-17 erg/s/cm2/A
+        flux = dat["col2"] * 1e-17 * u.erg / u.s / u.cm**2 / u.AA # in 1e-17 erg/s/cm2/A
         flux_err = dat["col3"]
 
         mask_1450 = (wl_rest > 1445) & (wl_rest < 1455)
         flux_mean = np.mean(flux[mask_1450])
         scale = flam / flux_mean
-        flux = (flux * scale * 1e17).value
+        flux = flux * scale
         # absorption of HI
         # TODO: simulate "real" absorption
         wl_lya = 1215.67 * (1 + redshift)
@@ -110,21 +116,23 @@ def simulate_Nlam(model='qso', redshift=None, M_1450=None,
     # wl_gpm = (sens.wave>1)
 
     # calculate the 1/S_lambda, the factor that converts the flux to N_lambda
-    factor = Flam_to_Nlam(sens.wave, sens.zeropoint).flatten()
+    S_lam_units = 1e-17 * u.erg / u.cm**2
+    factor = Flam_to_Nlam(sens.wave, sens.zeropoint).flatten() 
     func_factor = interpolate.interp1d(sens.wave.flatten(), factor)
 
     mask = (wl_obs > sens.wave[0]) & (wl_obs < sens.wave[-1])
     wl_obs_lris = wl_obs[mask]
-    Nlam = flux[mask] * func_factor(wl_obs_lris)
-    
+    Nlam = flux[mask] * func_factor(wl_obs_lris) / S_lam_units
+    Nlam = Nlam.to(1/u.AA/u.s).value # 1 / (Angstrom s)
+
     # TODO this is too twisted, should generalize this
     wl_lris, Nlam_lris = zfill(wl_obs_lris, Nlam, sens.wave[0])
 
     return wl_lris, Nlam_lris
 
-exptime = 20 # seconds
-# wave, Nlam = simulate_Nlam(model='qso', redshift=6.1, M_1450=-26)
-wave, Nlam = simulate_Nlam(model='star', star_scale=0.04, star_type='L0')
+exptime = 300 # seconds
+wave, Nlam = simulate_Nlam(model='qso', redshift=7.5, m_1450=21)
+# wave, Nlam = simulate_Nlam(model='star', star_scale=0.04, star_type='L0')
 
 # sci_path = '/Users/joe/python/PypeIt-development-suite/REDUX_OUT/keck_lris_red_mark4/long_600_10000_d680/Science/'
 sci_path = '../arxiv/LRIS_2203/LRIS_220306/reduced/all/Science/'
@@ -164,24 +172,23 @@ extract.extract_boxcar(spec2DObj.sciimg, spec2DObj.ivarmodel, gpm, spec2DObj.wav
 # display.show_trace(viewer, ch_skysub, sobjs_fake.TRACE_SPAT, 'fake object', color='orange')
 
 # 1) Interpolate the counts per second onto the sobjs_fake.BOX_WAVE
-
-# 2) Create a 2d image with an object profile comprising a Gaussian centered on TRACE_SPAT
-
-# 3) Force the extract_boxcar of the 2d image to equal to the total counts/per second/per real data pixel
-
-# 4) As a simple test you can simply add the 2d image to the noisy 2d skysubtracted image (think we showed in ginga above)
-# which will already give you an idea of what a high-z quasar looks like
-
 func_Nlamb = interpolate.interp1d(wave, Nlam)
 mask = (sobjs_fake.BOX_WAVE > wave[0]) & (sobjs_fake.BOX_WAVE < wave[-1])
 Nlam_boxwave = func_Nlamb(sobjs_fake.BOX_WAVE[mask])
 delta_wave = wvutils.get_delta_wave(sobjs_fake.BOX_WAVE[mask], (sobjs_fake.BOX_WAVE[mask] > 1.0))
 Npix_boxwave = Nlam_boxwave * exptime * delta_wave
 
-img_gaussians = gauss_comb(spec2DObj.sciimg[mask].shape, sobjs_fake.TRACE_SPAT[mask], sig=1)
+# 2) Create a 2d image with an object profile comprising a Gaussian centered on TRACE_SPAT
+# 3) Force the extract_boxcar of the 2d image to equal to the total counts/per second/per real data pixel
+# TODO: use seeing here
+img_gaussians = gauss_comb(spec2DObj.sciimg[mask].shape, sobjs_fake.TRACE_SPAT[mask], sig=0.86)
 flux_gaussians = moment1d(img_gaussians, sobjs_fake.TRACE_SPAT[mask], sobjs_fake.BOX_RADIUS*2, order=[0])[0]
 factor_fake = Npix_boxwave / flux_gaussians
+# TODO try numpy.einsum
 img_fake_tmp = (img_gaussians.T*factor_fake).T
+
+# 4) As a simple test you can simply add the 2d image to the noisy 2d skysubtracted image (think we showed in ginga above)
+# which will already give you an idea of what a high-z quasar looks like
 
 # Make a plot of fake quasar
 image = (spec2DObj.sciimg - spec2DObj.skymodel) * gpm
