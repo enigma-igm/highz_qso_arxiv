dc630d7db9cc052c65423d6e12a10db94b9000cb
topologyYDM <topologyYDM@gmail.com>
2022-09-19 20:21:16 +0200

    highz qso arxiv (corrected)

--- highz_qso_arxiv/arxiv/LRIS_2201/plot/plot.py
+++ highz_qso_arxiv/arxiv/LRIS_2201/plot/plot.py
@@ -1,36 +1,19 @@
+from re import template
 from highz_qso_arxiv.plot import plot_series
 
-"""
-    plot all without tellruic
-"""
-name_list = ['J0739+2328', 'J0759+2811', 'J0820+2412', 'J0831+2558',
+name_list = ['J0739+2328', 'J0759+2811', 'J0831+2558',
              'J0849+2601', 'J0936+3244', 'J0936+3346', 'J0938+3332',
              'J1003-2610', 'J1030-0031', 'J1145+0933', 'J1146+0039',
              'J1206-0051', 'J1209+0135', 'J1241-0134', 'J1246-3045',
              'J1256-0306', 'J1305-1549', 'J1326+0927']
 fits_list = [f"../reduced/all/{nm}/{nm}_coadd.fits" for nm in name_list]
-fits_list[10] = f"../reduced/all/coadd2d/{name_list[10]}_coadd.fits"
-fits_list[12] = f"../reduced/all/coadd2d/{name_list[12]}_coadd.fits"
-fits_list[16] = f"../reduced/all/coadd2d/{name_list[16]}_coadd.fits"
+fits_list[9] = f"../reduced/all/coadd2d/{name_list[9]}_coadd.fits"
+fits_list[11] = f"../reduced/all/coadd2d/{name_list[11]}_coadd.fits"
+fits_list[15] = f"../reduced/all/coadd2d/{name_list[15]}_coadd.fits"
 idx_list = [1 for i in range(len(name_list))]
-plot_series(name_list, fits_list, idx_list, display=False, save_file="LRIS_2201.pdf")
+template_list = [True for i in range(len(name_list))]
+plot_series(name_list, fits_list, idx_list, template_list=template_list, display=False, save_file="LRIS_2201.pdf")
 
-"""
-    plot all with telluric
-"""
-fits_list = [f"../reduced/all/{nm}/{nm}_coadd_tellcorr.fits" for nm in name_list]
-fits_list[10] = f"../reduced/all/coadd2d/{name_list[10]}_coadd_tellcorr.fits"
-fits_list[12] = f"../reduced/all/coadd2d/{name_list[12]}_coadd_tellcorr.fits"
-fits_list[16] = f"../reduced/all/coadd2d/{name_list[16]}_coadd_tellcorr.fits"
-idx_list = [1 for i in range(len(name_list))]
+fits_list = [f[:-5] + '_tellcorr' + f[-5:] for f in fits_list]
 telluric_list = [True for i in range(len(name_list))]
-plot_series(name_list, fits_list, idx_list, telluric_list=telluric_list, display=False, save_file="LRIS_2201_tellcorr.pdf")
-
-"""
-    plot J1206 and J1256
-"""
-name_list = ['J1206-0051', 'J1256-0306']
-fits_list = [f"../reduced/all/coadd2d/{nm}_coadd_tellcorr.fits" for nm in name_list]
-idx_list = [1 for i in range(len(name_list))]
-telluric_list = [True for i in range(len(name_list))]
-plot_series(name_list, fits_list, idx_list, telluric_list=telluric_list, display=False, save_file="J1206_J1256.pdf")
+plot_series(name_list, fits_list, idx_list, template_list=template_list, telluric_list=telluric_list, display=False, save_file="LRIS_2201_tellcorr.pdf")
--- highz_qso_arxiv/arxiv/LRIS_2203/plot/plot.py
+++ highz_qso_arxiv/arxiv/LRIS_2203/plot/plot.py
@@ -5,39 +5,35 @@ targets_0305 = ["J1100+0203", "J1143-0248", "J1200+0112", "J1223+0114",
                 "J1250+0347", "J1319+0101", "J1327-0207", "J1335+0103",
                 "J1355-0044", "J1355-0111", "J1356+0050", "J1358-0118",
                 "J1422+0307"]
-fits_list = [f"../LRIS_220305/reduced/all/{nm}/{nm}_coadd_tellcorr.fits" for nm in targets_0305]
-fits_list[0] = "../LRIS_220305/reduced/all/coadd2d/J1100+0203_coadd_tellcorr.fits"
-fits_list[2] = "../LRIS_220305/reduced/all/coadd2d/J1200+0112_coadd_tellcorr.fits"
-idx_list = [1 for i in range(len(targets_0305))]
-
-# a list of True with the same length as fits_list
-template_list = [True for i in range(len(fits_list))]
-template_list[5] = False
-telluric_list = [True for i in range(len(fits_list))]
-plot_series(targets_0305, fits_list, idx_list, smooth_window=3, 
-            template_list=template_list, telluric_list=telluric_list, display=False, save_file="LRIS_220305.pdf")
-
 targets_0306 = ["J0810+2352", "J0850+0146", "J0901+2906", "J0911+0022",
                 "J0947+0111", "J1153-2239", "J1233-2807", "J1412-2757",
                 "J1425+0004", "J1427+0202", "J1458+1012", "J1510-0144", 
                 "J1514-0121", "J1522+0051", "J1534+0046", "J1655-0051", "J1724+3718"]
-fits_list = [f"../LRIS_220306/reduced/all/{nm}/{nm}_coadd_tellcorr.fits" for nm in targets_0306]
-fits_list[9] = "../LRIS_220306/reduced/all/coadd2d/J1427+0202_coadd_tellcorr.fits"
-idx_list = [1 for i in range(len(targets_0306))]
+targets_list = targets_0305 + targets_0306
+targets_list.sort()
 
+fits_list = []
+for nm in targets_list:
+    if nm in targets_0305:
+        fits_list.append(f"../LRIS_220305/reduced/all/{nm}/{nm}_coadd.fits")
+    else:
+        fits_list.append(f"../LRIS_220306/reduced/all/{nm}/{nm}_coadd.fits")
+fits_list[targets_list.index('J1100+0203')] = "../LRIS_220305/reduced/all/coadd2d/J1100+0203_coadd.fits"
+fits_list[targets_list.index('J1200+0112')] = "../LRIS_220305/reduced/all/coadd2d/J1200+0112_coadd.fits"
+fits_list[targets_list.index('J1427+0202')] = "../LRIS_220306/reduced/all/coadd2d/J1427+0202_coadd.fits"
+idx_list = [1 for i in range(len(targets_list))]
 template_list = [True for i in range(len(fits_list))]
-template_list[2] = False
-template_list[10] = False
-template_list[-1] = False
-telluric_list = [True for i in range(len(fits_list))]
-plot_series(targets_0306, fits_list, idx_list, smooth_window=3,
-            template_list=template_list, telluric_list=telluric_list, display=False, save_file="LRIS_220306.pdf")
+template_list[targets_list.index('J0901+2906')] = False
+template_list[targets_list.index('J1319+0101')] = False
+template_list[targets_list.index('J1458+1012')] = False
+template_list[targets_list.index('J1724+3718')] = False
 
-qso_targets = ["J1458+1012", "J1319+0101", "J1724+3718", "J0901+2906"]
-fits_list = [f"../LRIS_220306/reduced/all/{nm}/{nm}_coadd_tellcorr.fits" for nm in qso_targets]
-fits_list[1] = f"../LRIS_220305/reduced/all/J1319+0101/J1319+0101_coadd_tellcorr.fits"
-idx_list = [1 for i in range(len(qso_targets))]
+telluric_list = [False for i in range(len(fits_list))]
 
-template_list = [False for i in range(len(fits_list))]
+plot_series(targets_list, fits_list, idx_list, smooth_window=3, 
+            template_list=template_list, telluric_list=telluric_list, display=False, save_file="LRIS_2203.pdf")
+
+fits_list = [f[:-5] + '_tellcorr' + f[-5:] for f in fits_list]
 telluric_list = [True for i in range(len(fits_list))]
-plot_series(qso_targets, fits_list, idx_list, template_list=template_list, telluric_list=telluric_list, display=False, save_file="LRIS_2203_qso.pdf")
+plot_series(targets_list, fits_list, idx_list, smooth_window=3,
+            template_list=template_list, telluric_list=telluric_list, display=False, save_file="LRIS_2203_tellcorr.pdf")
